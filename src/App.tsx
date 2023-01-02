6d2dd581669dba2a6a3c92927f3aa5da73fb98a0
Thomas Fouquert <tfouquert@gmail.com>
2023-01-02 13:49:54 +0100

    add urql graphql client for react

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,25 @@ import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import { Home } from "./pages/Home";
+import { createClient, Provider } from "urql";
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <ColorModeSwitcher />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  </ChakraProvider>
+  <Provider value={client}>
+    <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
 );
