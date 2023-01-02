c5a3b93a0a113961592fe71deda7db535325dff4
Thomas Fouquert <tfouquert@gmail.com>
2023-01-02 21:01:03 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,78 @@
 import * as React from "react";
-import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import { Home } from "./pages/Home";
-import { createClient, Provider } from "urql";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
 import Login from "./pages/Login";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "./generated/graphql";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  queryInput: QueryInput,
+  result: any,
+  fn: (result: Result, query: Query) => Query
+) {
+  return cache.updateQuery(
+    queryInput,
+    (data) => fn(result, data as any) as any
+  );
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 export const App = () => (
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -50,7 +50,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
   }
 
   return (
-    <Flex zIndex={1} position="sticky" top={0} bg="tomato" p={4}>
+    <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
       <Flex flex={1} m="auto" align="center" maxW={800}>
         <Link to={"/"}>
           <Heading>Reddit Clone</Heading>
