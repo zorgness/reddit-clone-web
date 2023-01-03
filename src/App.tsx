e9837f11c7a1aa6a4e3d454712293b3dc26faace
Thomas Fouquert <tfouquert@gmail.com>
2023-01-03 11:18:26 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,17 @@
-import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
+import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
-import { cacheExchange } from "@urql/exchange-graphcache";
 import Login from "./pages/Login";
-import {
-  LoginMutation,
-  MeDocument,
-  MeQuery,
-  RegisterMutation,
-} from "./generated/graphql";
-import { betterUpdateQuery } from "./utils/betterUpdateQuery";
-
-// const client = createClient({
-//   url: "http://localhost:4000/graphql",
-//   fetchOptions: {
-//     credentials: "include",
-//   },
-//   exchanges: [
-//     dedupExchange,
-//     cacheExchange({
-//       updates: {
-//         Mutation: {
-//           login: (_result, args, cache, info) => {
-//             betterUpdateQuery<LoginMutation, MeQuery>(
-//               cache,
-//               { query: MeDocument },
-//               _result,
-//               (result, query) => {
-//                 if (result.login.errors) {
-//                   return query;
-//                 } else {
-//                   return {
-//                     me: result.login.user,
-//                   };
-//                 }
-//               }
-//             );
-//           },
-//           register: (_result, args, cache, info) => {
-//             betterUpdateQuery<RegisterMutation, MeQuery>(
-//               cache,
-//               { query: MeDocument },
-//               _result,
-//               (result, query) => {
-//                 if (result.register.errors) {
-//                   return query;
-//                 } else {
-//                   return {
-//                     me: result.register.user,
-//                   };
-//                 }
-//               }
-//             );
-//           },
-//         },
-//       },
-//     }),
-//     fetchExchange,
-//   ],
-// });
+import Register from "./pages/Register";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home pageProps={undefined} />} />
+        <Route path="/register" element={<Register pageProps={undefined} />} />
+        <Route path="/login" element={<Login pageProps={undefined} />} />
       </Routes>
     </BrowserRouter>
   </ChakraProvider>
