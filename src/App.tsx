efa776b92d76461b827b67e296214f02a5f6b8df
Thomas Fouquert <tfouquert@gmail.com>
2023-01-03 12:31:39 +0100

    userEmail for change password

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ChangePassword from "./pages/change-password/[token]";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,6 +13,10 @@ export const App = () => (
         <Route path="/" element={<Home pageProps={undefined} />} />
         <Route path="/register" element={<Register pageProps={undefined} />} />
         <Route path="/login" element={<Login pageProps={undefined} />} />
+        <Route
+          path="/change-password/:token"
+          element={<ChangePassword token={""} />}
+        />
       </Routes>
     </BrowserRouter>
   </ChakraProvider>
--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -1,24 +1,66 @@
 import React from "react";
+import { NextPage } from "next";
+import { FormControl, Button, Box } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
 
-// declare global {
-//   namespace React {
-//   interface FunctionComponent<P = {}> {
-//   getInitialProps(): string;
-//   }
-//   }
-//   }
-// type queryProps = {
-//   query: string;
-// }
+import { InputField } from "../../components/InputField";
+import { Wrapper } from "../../components/Wrapper";
+import { toErrorMap } from "../../utils/toErrorMap";
+import { useParams } from "react-router-dom";
 
-// const ChangePassword: React.FC<{ token: string }> = ({ token }) => {
-//   return <div>token is {token}</div>;
-// };
+const ChangePassword: NextPage<{ token: string }> = () => {
+  const params = useParams();
+  console.log(params.token);
+  return (
+    <Wrapper variant={"small"}>
+      <Box textAlign={["center"]}>
+        <h1>Change Password</h1>
+      </Box>
 
-// ChangePassword.getInitialProps = ({query}) => {
-//   return {
-//     token: query.token as string,
-//   }
-// }
+      <Formik
+        initialValues={{ newPassword: "" }}
+        onSubmit={() => console.log(params.token)}
+        // onSubmit={async (values, { setErrors }) => {
+        //   // const response = await login(values);
+        //   // if (response.data?.login.errors) {
+        //   //   setErrors(toErrorMap(response.data?.login.errors));
+        //   // } else if (response.data?.login.user) {
+        //   //   // navigate("/");
+        //   // }
+        // }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <FormControl>
+              <Box mt={4}>
+                <InputField
+                  name="password"
+                  placeholder="password"
+                  label="Password"
+                  type="password"
+                />
+              </Box>
 
-// export default ChangePassword;
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={isSubmitting}
+                type="submit"
+              >
+                Login
+              </Button>
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string,
+  };
+};
+
+export default ChangePassword;
