62d600e01e3d7ced8a5debbfd79d662ac414a680
Thomas Fouquert <tfouquert@gmail.com>
2023-01-03 01:07:14 +0100

    createUrqlClient

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 interface HomeProps {}
 
-export const Home: React.FC<HomeProps> = ({}) => {
+const Home: React.FC<HomeProps> = ({}) => {
   return (
     <div>
       <NavBar />
     </div>
   );
 };
+
+export default withUrqlClient(createUrqlClient)(Home);
