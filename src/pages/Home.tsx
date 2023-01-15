04383bae10d898a5b62ed92605ebc47b93b8a710
Thomas Fouquert <tfouquert@gmail.com>
2023-01-15 13:48:35 +0100

    get post creator

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,8 +20,6 @@ const Home: React.FC<HomeProps> = () => {
     variables,
   });
 
-  console.log(data);
-
   if (!fetching && !data) {
     return (
       <div>
@@ -37,12 +35,12 @@ const Home: React.FC<HomeProps> = () => {
         <div>...loading</div>
       ) : (
         <Stack spacing={8}>
-          {data!.posts.posts.map(({ _id, title, textSnippet }) => {
+          {data!.posts.posts.map(({ _id, title, textSnippet, creator }) => {
             return (
               <Box key={_id} p={5} shadow="md" borderWidth="1px">
                 <Heading fontSize="xl">{title}</Heading>
                 <Text mt={4}>{textSnippet}...</Text>
-                <Text mt={4}>{}</Text>
+                <Text mt={4}>{creator.username}</Text>
               </Box>
             );
           })}
